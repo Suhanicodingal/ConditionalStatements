829e93189b1fdf6c654786a637ac4f8d8af300d4
Suhanicodingal <amritasrivastava849@gmail.com>
2025-02-02 13:59:10 +0530

    Add files via upload

---
+++ SummerTimes.py
@@ -0,0 +1,11 @@
+summer_times = str(input("Please Enter Season Main Thing Name:  "))
+
+temp_measure = float(input("Please Enter Season Temperature:  "))
+
+beauty_things = str(input("Please Enter the Things you like in the summer season:  "))
+
+if (summer_times < temp_measure) :
+     times = summer_times + beauty_things
+     print("Beautiful Season = {str}". format(summer_times))
+else
+     print( None Favourite Season!!!)     
